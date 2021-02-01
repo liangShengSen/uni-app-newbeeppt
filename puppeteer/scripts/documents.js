efda98a93a3c4e2d9f7f4dc231d45972ed221a64
liangshengsen <1213509006@qq.com>
2021-02-01 08:40:06 +0800

    merge

--- puppeteer/scripts/documents.js
+++ puppeteer/scripts/documents.js
@@ -9,7 +9,7 @@ const dayjs = require('dayjs');
 	try {
 		const page = await browser.newPage();
 		await page.setDefaultNavigationTimeout(0);
-		await page.goto('http://www.pptok.com/kejian/mulu/11.html');
+		await page.goto('http://www.pptok.com/kejian/mulu/204.html');
 		let arr = await page.$$eval('#mulu .mldy h2 a', (a) =>
 			a.map((v) => {
 				return {
@@ -36,7 +36,11 @@ const dayjs = require('dayjs');
 						await page.goto(pagesLinks[j]);
 						let arr = await page.$$eval('#tg_box_con > li > a', (a) =>
 							a.map((v) => {
-								return v.href;
+								let img = $(v).find('.topic > img').attr('src')
+								return {
+									href: v.href,
+									cover: `http://www.pptok.com${img}`,
+								}
 							})
 						);
 						list = [...list, ...arr];
@@ -44,47 +48,75 @@ const dayjs = require('dayjs');
 				} else {
 					list = await page.$$eval('#tg_box_con > li > a', (a) =>
 						a.map((v) => {
-							return v.href;
+							let img = $(v).find('.topic > img').attr('src')
+							return {
+								href: v.href,
+								cover: `http://www.pptok.com${img}`,
+							}
 						})
 					);
 				}
 				arr[i]['list'] = list;
 				for (let n = 0; n < arr[i]['list'].length; n++) {
 					console.log(arr[i]['name'])
-					console.log(arr[i]['list'][n])
-					if (arr[i]['list'][n] != 'http://www.pptok.com/kejian/jingyanggangppt/index_5.html') {
-						await page.goto(arr[i]['list'][n]);
+					try {
+						await page.goto(arr[i]['list'][n]['href']);
 						let info = {
 							name: arr[i].name,
 							data: {
 								title: await page.$eval('#main h1', (h) => h.innerHTML),
 								intro: await page.$eval('#soft-intro p:nth-child(4)', (p) => p.innerHTML),
-								fileSize: await page.$eval('.ver-chose', (span) => span.innerHTML),
-								priviewImg: await page.$eval('#soft-intro img', (img) => img.src),
-								stage_id: '1',
-								subject_id: '2',
-								version_id: '1001',
-								book_id: '10013',
-								chapter_id: '',
-								grade_id: '1',
-								term: "2",
-								user_id: '001',
-								user_name: 'Sam',
+								file_size: await page.$eval('.ver-chose', (span) => span.innerHTML),
+								priview_imgs: [await page.$eval('#soft-intro img', (img) => img.src)],
+								cover_img: arr[i]['list'][n]['cover'],
+								author: {
+								    "id": "001",
+								    "name": "Sam"
+								},
+								stage: {
+									"id": "1",
+									"name": "小学"
+								},
+								subject: {
+									"id": "2",
+									"name": "语文"
+								},
+								version:{
+									"id":"1000",
+									"name":"人教部编版"
+								},
+								book:{
+									"id":"10002",
+									"name":"二年级上册"
+								},
+								grade: {
+									"id": "2",
+									"name": "二年级"
+								},
+								chapter:{
+									"id":"",
+									"name": ""
+								},
+								term: "1",
 								year: '2021',
 								rank: '0',
 								price: 5,
 								status: '1',
 								download_num: 0,
-								create_at: dayjs().format('YYYY-MM-DD'),
-								update_at: null,
+								created_at: dayjs().format('YYYY-MM-DD'),
+								type: "ppt",
+								type_label: "课件"
 							},
 						};
 						docsArr.push(info);
+					}catch(err) {
+						console.log(`错误链接：${arr[i]['list'][n]['href']}`)
+						console.log(err)
 					}
 				}
 			}
 		}
-		fs.writeFileSync('./json/chapters.json', JSON.stringify(docsArr));
+		fs.writeFileSync('./json/temp.json', JSON.stringify(docsArr));
 	} catch (e) {
 		console.log('err', e);
 	} finally {
